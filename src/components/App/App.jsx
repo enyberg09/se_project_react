a0c676ffd93b3b7e25fccbbd26428fa5c70348e4
elissanyberg <esnyberg08@gmail.com>
2025-06-09 12:54:26 -0400

    Basic Cards Setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import "../../vendor/fonts.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "" });
+
   return (
     <div className="page">
       <div className="page__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
---
+++ src/components/ModalWithForm.jsx
@@ -0,0 +1,5 @@
+function ModalWithForm() {
+  return <></>;
+}
+
+export default ModalWithForm;
